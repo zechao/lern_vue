511cf0d8ef62ba03620901b9b3e2e6e58e800358
zechao <zechao158@gmail.com>
2023-10-19 02:54:46 +0200

    vue3 init and ref

---
+++ vue3_learn/01_vue3_setup/App.vue
@@ -0,0 +1,35 @@
+<template>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<h1>Hello {{ name }}</h1>
+	<input v-model="name" />
+	<button @click="sayHello">sayHello</button>
+</template>
+
+<script>
+export default {
+	name: "App",
+	setup() {
+		let name = "Zechao";
+
+		function sayHello() {
+			alert(`Hello ${name}!`);
+		}
+
+		return {
+			name,
+			sayHello,
+		};
+	},
+};
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
